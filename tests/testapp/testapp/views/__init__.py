42bb9d686af35746e26e70b58ebc379c681f734d
Maxim Avanov <maxim.avanov@gmail.com>
2014-01-06 23:02:02 +0400

    Update testapp's views config

--- tests/testapp/testapp/views/__init__.py
+++ tests/testapp/testapp/views/__init__.py
@@ -4,3 +4,13 @@ from rhetoric import view_config
 @view_config(route_name='test.new.routes')
 def default_view(request):
     return {}
+
+
+@view_config(route_name='test.new.routes', request_method='POST')
+def submit_form_view(request):
+    return {}
+
+
+@view_config(route_name='test.new.routes', request_method='POST', api_version='1.0')
+def api_v1_submit_form_view(request):
+    return {}
--- tests/url_tests.py
+++ tests/url_tests.py
@@ -5,6 +5,7 @@ class URLTest(BaseTestCase):
 
     def test_routes(self):
         from tests.testapp.testapp import urls
+
         pattern_list = urls.urlpatterns
         route_names = {p.name for p in pattern_list}
         self.assertIn('test.new.routes', route_names)
