35d4bc796f27bdf5aea7c3dea83c581260a4669a
Maxim Avanov <maxim.avanov@gmail.com>
2014-01-07 12:17:23 +0400

    Use patterns with explicit length

--- tests/testapp/testapp/blog/__init__.py
+++ tests/testapp/testapp/blog/__init__.py
@@ -1,4 +1,4 @@
 def includeme(config):
-    config.add_route('test.new.routes', '/test/new/routes/{param:[a-z]+}')
+    config.add_route('test.new.routes', '/test/new/routes/{param:[a-z]{1,}}')
     config.add_route('blog.page', '/page/{page_slug}')
 
