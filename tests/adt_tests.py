238854b2bd5c4c3b6b24e2729ce4c05877bd05cb
Maxim Avanov <maxim.avanov@gmail.com>
2014-04-17 01:53:07 +0400

    Do not test adt metadata.

--- tests/adt_tests.py
+++ tests/adt_tests.py
@@ -6,6 +6,5 @@ class ADTTest(BaseTestCase):
     def test_metadata(self):
         from tests.testapp.testapp import types
 
-        assert types.Language.ENGLISH.variant_of.__adt__['variants'] == {'ENGLISH', 'GERMAN'}
         assert types.Language.ENGLISH.variant_of == types.Language
         assert types.Language.ENGLISH.value == 'en'
