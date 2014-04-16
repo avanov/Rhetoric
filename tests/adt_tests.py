bb8282f3d5befd285eb73efffef16ef2fd6b5a3c
Maxim Avanov <maxim.avanov@gmail.com>
2014-04-17 00:21:44 +0400

    Fix Python3 builds

--- tests/adt_tests.py
+++ tests/adt_tests.py
@@ -4,7 +4,7 @@ from . import BaseTestCase
 class ADTTest(BaseTestCase):
 
     def test_metadata(self):
-        from testapp.testapp import types
+        from tests.testapp.testapp import types
 
         assert types.Language.ENGLISH.variant_of.__adt__['variants'] == {'ENGLISH', 'GERMAN'}
         assert types.Language.ENGLISH.variant_of == types.Language
