848a64de9f0d2ef1e2bdabaf329143640ebdaf37
Maxim Avanov <maxim.avanov@gmail.com>
2014-04-16 23:12:59 +0400

    Update ADT API

---
+++ tests/adt_tests.py
@@ -0,0 +1,11 @@
+from . import BaseTestCase
+
+
+class ADTTest(BaseTestCase):
+
+    def test_metadata(self):
+        from testapp.testapp import types
+
+        assert types.Language.ENGLISH.variant_of.__adt__['variants'] == {'ENGLISH', 'GERMAN'}
+        assert types.Language.ENGLISH.variant_of == types.Language
+        assert types.Language.ENGLISH.value == 'en'
---
+++ tests/testapp/testapp/articles/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from ..types import Language
+
+
+class AbstractRegionalArticle(models.Model):
+    title = models.CharField(max_length=250, null=False, default='')
+    text = models.TextField(null=False, default='')
+
+    class Meta:
+        abstract = True
+
+
+@Language.ENGLISH('db:articles')
+class EnglishArticle(AbstractRegionalArticle):
+    pass
+
+@Language.GERMAN('db:articles')
+class GermanArticle(AbstractRegionalArticle):
+    pass
+
+@Language.GERMAN('db:articles')
+class OtherArticles(AbstractRegionalArticle):
+    pass
