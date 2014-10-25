af29cea85eea0126857e15d7189e19cde880fff7
Maxim Avanov <maxim.avanov@gmail.com>
2014-10-25 21:33:24 +0400

    Default request encoding is utf-8

--- rhetoric/request.py
+++ rhetoric/request.py
@@ -9,4 +9,4 @@ from .compat import text_
 
 class JsonBodyProperty(SimpleLazyObject):
     def __init__(self, request):
-        super(JsonBodyProperty, self).__init__(lambda: json.loads(text_(request.read(), request.encoding)))
+        super(JsonBodyProperty, self).__init__(lambda: json.loads(text_(request.read(), request.encoding or 'utf-8')))
