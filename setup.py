ebf003c9f2a46765e72a445b9507151cc43f9156
Maxim Avanov <maxim.avanov@gmail.com>
2015-04-29 16:07:03 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='Rhetoric',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Django>=1.4,<1.8',
